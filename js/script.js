9c5b0f4d803b4f60633918c3e4d225a4f93a9ebf
Geccles <geccles01@gmail.com>
2016-01-06 20:04:16 -0500

    markers up and info window
    lots of stuff done for the view

--- js/script.js
+++ js/script.js
@@ -1,92 +1,77 @@
+var modelLoaded = false;
+var modelWineries = [];
 
-function loadData()
-{
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+// API for winery list
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  dataType: "json",
+  "url": "http://eccleshome.com/winery-project/api.php/wineries",
+  "method": "GET"
+};
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
-
-    // load streetview
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ',' + cityStr;
-    console.log(address);
-
-    $greeting.text('So, you want to live at' + address + '?');
-
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + ' ';
-    var appendMsg = '<img class="bgimg" src= "' + streetviewUrl + '" >';
-    console.log(appendMsg);
-    $body.append('<img class="bgimg" src= "' + streetviewUrl + '" >');
-
-    var URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&api-key=eeaee8614076b91d1e6e6b2e330ef4e1:9:36098306";
-
+$.ajax(settings).done(function(response) {
+  console.log(response);
+  for (var i = 0; i < response.length; i++) {
+    modelWineries.push(response[i]);
+  }
+  modelLoaded = true;
+});
 
 //AJAX statement to get Weather Underground key: bf48407e50740efb
+var URL = "http://api.wunderground.com/api/bf48407e50740efb/conditions/q/CA/San_Francisco.json";
 
-
-
-  var URL = "http://api.wunderground.com/api/bf48407e50740efb/conditions/q/CA/San_Francisco.json";
-
-//AJAX statement to get the NY Times Article
-    $.getJSON(URL, function(data)
-    {
-      var list = [];
-      console.log(data);
-      var articles = data.response.docs;
-      for (var i = 0; i < 10; i++)
-    {
-        var article = articles[i];
-
-        list.push("<li>" + '<a href="'+ article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + "</li>");
-    }
-      $nytHeaderElem.append(list);
-    }).error(function(e)
-    {
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
-
-
-//AJAX request to get Wikipedia links
-  var wikiUrl = 'http://en.wikiabcdefgpedia.org/w/api.php?action=opensearch&search= ' + cityStr + ' &format=json&callback=wikiCallback';
-  var wikiRequestTimeout = setTimeout(function()
-  {
-    $wikiElem.text("Failed to Get Wikipedia Resources");
-  }, 500);
-  $.ajax(
-    {
-      url: wikiUrl,
-      dataType: "jsonp",
-      //jsnop: "callback",
-      success: function (response)
-      {
-        var articleList = response[1];
-
-        for (var i = 0; i < articleList.length; i++)
-        {
-          articleStr = articleList[i];
-          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-          $wikiElem.append('<li><a href= "' + url +'" >' + articleStr + '</a></li>');
-        }
-          clearTimeout(wikiRequestTimeout);
-      }
-    });
-      return false;
-}
-
-
+//this loads the map
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 38.5000, lng: -122.3200},
+    center: {
+      lat: 38.5000,
+      lng: -122.3200
+    },
     zoom: 10,
-    disableDefaultUI: true
+    disableDefaultUI: false
+  });
+  //this creates a marker
+  var placeMarker = function(myLatLng, name) {
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: name
+    });
+
+    //this makes an info window for each marker
+    var contentString = '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h3 id="firstHeading" class="firstHeading">' + name + '</h3>'+
+          '</div>';
+
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+
+  };
+
+//this preps the markers to be made
+  var makeMarkers = function ()
+  {
+    for (var i = 0; i < modelWineries.length; i++)
+    {
+      placeMarker(new google.maps.LatLng(modelWineries[i].latitude, modelWineries[i].longitude), modelWineries[i].name);
+    }
+  };
+
+//checkes to see that ajax call completes
+  $(document).ajaxComplete(function(event, xhr, settings)
+  {
+    if (settings.url === "http://eccleshome.com/winery-project/api.php/wineries")
+    {
+      makeMarkers();
+    }
   });
 }
-
-$('#form-container').submit(loadData);
