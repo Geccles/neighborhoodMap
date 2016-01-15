3d07829c6f5157523f8a2d14fc1baa966d4f670f
Geccles <geccles01@gmail.com>
2016-01-14 21:07:05 -0500

    Foursquare for photos
    Finished with the api for foursquare…added an info bubble plus a second
    tab for photos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var modelLoaded = false;
 var modelWineries = ko.observableArray([]);
 var filter = ko.observable('');
-var openInfoWindow = null;
+var openInfoBubble = null;
 var currentMarker = null;
 
 // API for winery list
@@ -44,6 +44,36 @@ filteredItems.subscribe(function(newList) {
   }
 });
 
+//retrieves photos from foursquare from each winery
+var photoWine = function(venueId, infoBubble, indexTab) {
+  var prefix = "https://api.foursquare.com/v2/venues/";
+  var suffix = "/photos?client_id=0G5RS5KJQLRKGBBO5WZUH1H1SNMWBNWBTENS03AKKAB0ZA30&client_secret=DCIZDOFW11PNDHPODTKNZFJI0YL0VKOC32FTXPMBYLEZJLN1&v=20130815";
+
+  var uri = prefix + venueId + suffix;
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": uri,
+    "method": "GET"
+  };
+
+  $.ajax(settings).done(function(results) {
+    var count = results.response.photos.count;
+    var prefix, suffix, uri;
+    var contentString = "";
+    for (var i = 0; i < count; i++) {
+      prefix = results.response.photos.items[i].prefix;
+      suffix = results.response.photos.items[i].suffix;
+      uri = prefix + "width300" + suffix;
+      contentString += '<img src="' + uri + '" alt="Foursquare photo">';
+      contentString += '<br>';
+      console.log(contentString);
+      infoBubble.updateTab(indexTab, "Photos", contentString);
+    }
+  });
+};
+
 //if data doesn't load alert the viewer
 var myTimeOut = setTimeout(function() {
   alert('Sorry, We were unable to retrieve this data. Please check your wifi...I bet it is not on.');
@@ -76,7 +106,7 @@ function initMap() {
   //this creates a marker with a letter inside it
 
   var image = 'images/wine-icon-2.png';
-  var placeMarker = function(myLatLng, name, phoneNumber, address, website) {
+  var placeMarker = function(myLatLng, name, phoneNumber, address, website, venueId) {
     var marker = new google.maps.Marker({
       position: myLatLng,
       animation: google.maps.Animation.DROP,
@@ -85,7 +115,7 @@ function initMap() {
       icon: image
     });
 
-    //this makes an info window for each marker
+    //this makes an info Bubble for each marker
     var contentString = '<div id="content">' +
       '<div id="siteNotice">' +
       '</div>' +
@@ -94,34 +124,45 @@ function initMap() {
       '<h5 id="phoneNumber" class="number">' + phoneNumber + '</h5>' + '</div>' +
       '<h5 id="address" class="address1">' + address + '</h5>' + '</div>';
 
-    var infoWindow = new google.maps.InfoWindow({
-      content: contentString
+    //creating infobubble and style
+    var infoBubble = new InfoBubble({
+      minHeight: 250,
+      maxHeight: 300,
+      minWidth: 325,
+      maxWidth: 400
     });
 
-    //opens infowindow
+    //first tab for content on InfoBubble
+    infoBubble.addTab('Winery', contentString);
+
+    //second tab for photos on InfoBubble
+    infoBubble.addTab('Photos', "Here are photos");
+    photoWine(venueId, infoBubble, 1);
+
+    //opens infoBubble
     marker.addListener('click', clickMarker);
 
     function clickMarker() {
-      if (openInfoWindow) {
-        openInfoWindow.close();
+      if (openInfoBubble) {
+        openInfoBubble.close();
       }
       if (currentMarker) {
         currentMarker.setIcon('images/wine-icon-2.png');
       }
       marker.setIcon('images/wine-icon.png');
       positionMap(marker.getPosition(), 13);
-      infoWindow.open(map, marker);
-      openInfoWindow = infoWindow;
+      infoBubble.open(map, marker);
+      openInfoBubble = infoBubble;
       currentMarker = marker;
     }
 
-    //closes infowindow
-    infoWindow.addListener('closeclick', function() {
-      if (openInfoWindow) {
-        openInfoWindow.close();
+    //closes infoBubble
+    infoBubble.addListener('closeclick', function() {
+      if (openInfoBubble) {
+        openInfoBubble.close();
       }
       marker.setIcon('images/wine-icon-2.png');
-      openInfoWindow = null;
+      openInfoBubble = null;
       map.setCenter(new google.maps.LatLng(38.5000, -122.3200));
       map.setZoom(10);
     });
@@ -144,7 +185,7 @@ function initMap() {
   var makeMarkers = function() {
     var newMarkerObj;
     for (var i = 0; i < modelWineries().length; i++) {
-      newMarkerObj = placeMarker(new google.maps.LatLng(modelWineries()[i].latitude, modelWineries()[i].longitude), modelWineries()[i].name, modelWineries()[i].phoneNumber, modelWineries()[i].address, modelWineries()[i].website);
+      newMarkerObj = placeMarker(new google.maps.LatLng(modelWineries()[i].latitude, modelWineries()[i].longitude), modelWineries()[i].name, modelWineries()[i].phoneNumber, modelWineries()[i].address, modelWineries()[i].website, modelWineries()[i].venueId);
       modelWineries()[i].marker = newMarkerObj.marker;
       modelWineries()[i].clickMarker = newMarkerObj.clickMarker;
     }
