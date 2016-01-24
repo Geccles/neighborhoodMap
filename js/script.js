07616cca330d2fb0771f55079bb5dca03a7f33e2
Geccles <geccles01@gmail.com>
2016-01-24 18:09:13 -0500

    new view
    new styling to look good on phone, tablet, and computer…

--- js/script.js
+++ js/script.js
@@ -298,15 +298,3 @@ if (modelLoaded) {
     }
   });
 }
-
-//Navigation Menu Slider
-$(document).ready(function() {
-  $('#nav-expander').on('click', function(e) {
-    e.preventDefault();
-    $('body').toggleClass('nav-expanded');
-  });
-  $('#nav-close').on('click', function(e) {
-    e.preventDefault();
-    $('body').removeClass('nav-expanded');
-  });
-});
